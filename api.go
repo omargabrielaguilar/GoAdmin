0db2f1d9bf462df951c3bab75273048fb5778441
omar <saucyfunnel@gmail.com>
2023-09-25 08:46:18 -0500

    api.go resume

--- api.go
+++ api.go
@@ -1,19 +1,30 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
-type apiFunc func(http.ResponseWriter, *http.Request) error 
+func WriteJSON(w http.ResponseWriter, status int, v any) error {
+	w.WriteHeader(status)
+	w.Header().Set("Content Type", "application/json")
+	return json.NewEncoder(w).Encode(w)
+}
+
+type apiFunc func(http.ResponseWriter, *http.Request) error
+
+type ApiError struct {
+	Error string
+}
 
 func makeHTTPHandleFunc(f apiFunc) http.HandlerFunc {
-  return func(w http.ResponseWriter, r *http.Request) {
-    if err := f(w,r); err != nil {
-      // handle the error
-    }
-  }
+	return func(w http.ResponseWriter, r *http.Request) {
+		if err := f(w, r); err != nil {
+			WriteJSON(w, http.StatusBadRequest, ApiError{Error: err.Error()})
+		}
+	}
 }
 
 type APIServer struct {
@@ -26,12 +37,12 @@ func NewAPIServer(listenAddr string) *APIServer {
 	}
 }
 
-func (s *APIServer) Run(){
-  router := mux.NewRouter()
-  router.HandleFunc("/account", s.handleAccount)
+func (s *APIServer) Run() {
+	router := mux.NewRouter()
+	router.HandleFunc("/account", makeHTTPHandleFunc(s.handleAccount))
 }
 
-func (s *APIServer) handleAccount(w http.ResponseWriter, r *http.Request) error{
+func (s *APIServer) handleAccount(w http.ResponseWriter, r *http.Request) error {
 	return nil
 }
 
